c297ba31e7912fbe9ffc94b937b1af854fdf9556
zhijie <guszhiwe@student.gu.se>
2021-12-14 11:19:12 +0100

    update frontend code

--- client/stock/urls.py
+++ client/stock/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     url(r'^stock/$', views.stock, name='stock'),
+    path('predict', views.predict, name='predict'),
 ]
