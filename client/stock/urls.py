95474f62fb36270117bfeede6f1ad8e60bd384a0
Eric Hallberg <gushalerb@student.gu.se>
2021-12-14 11:33:19 +0100

    added urls mapping

--- client/stock/urls.py
+++ client/stock/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^stock/$', views.stock, name='stock'),
+    path('predict', views.predict, name='predict'),
 ]
