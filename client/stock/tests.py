fee725ae025209515e34d2fa044669f515487188
Eric Hallberg <gushalerb@student.gu.se>
2022-01-03 11:38:09 +0100

    fixed column test for topFiveFeats

--- client/stock/tests.py
+++ client/stock/tests.py
@@ -2,20 +2,11 @@
 from django.test import TestCase
 import pandas as pd
 
+
 # Create your tests here.
-def test_cleaned_data_columns(csvfile):
-    df = pd.read_csv (r'../client/data/balance-sheet.csv', sep=',')
-
-    actual = []
-    expected = ["timestamp", "symbol", "1m","reportedEPS","totalNonCurrentAssets","depreciation",
-               "proceedsFromRepaymentsOfShortTermDebt","currentAccountsPayable"]
-    for col in df.columns:
-        actual.append(col)
-
-    if set(actual) == set(expected):
-        return True
-    else:
-        return False
-
-test_cleaned_data_columns('topFiveFeats.csv')
-
+class Column(TestCase):
+    def test_columns_topfivefeats(self):
+        df = pd.read_csv(r'../client/data/topFiveFeats.csv', sep=',')
+        expected = ["timestamp", "symbol", "1m", "reportedEPS", "totalNonCurrentAssets", "depreciation",
+                    "proceedsFromRepaymentsOfShortTermDebt", "currentAccountsPayable"]
+        self.assertTrue(set(expected) <= set(df.columns))
