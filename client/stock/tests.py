4fa0adc3dc61ab02b88feaed1b711cf7743f9023
samuelg <gusgunsah@student.gu.se>
2022-01-04 23:01:47 +0100

    Add test case that asserts if file is created

--- client/stock/tests.py
+++ client/stock/tests.py
@@ -1,6 +1,11 @@
 
 from django.test import TestCase
+from h5py._hl import dataset
+from tensorflow.python.keras.backend import tile
+from stock.models import AiModel, DataSet
+import stock.admin as admin
 import pandas as pd
+from os.path import exists
 
 # Create your tests here.
 def test_cleaned_data_columns(csvfile):
@@ -18,4 +23,25 @@ def test_cleaned_data_columns(csvfile):
         return False
 
 test_cleaned_data_columns('topFiveFeats.csv')
+
+class AiModelTestCase(TestCase):
+    def setUp(self):
+        
+        # create dataset
+        testDataset = DataSet.objects.create(title='testData.csv')
+
+        # Create AiModel
+        AiModel.objects.create(title='UnitModel', deployed=True, dataset=testDataset)
+
+    def test_train_model_saves_file(self):
+        testModel = AiModel.objects.get(id=1)
+
+        # Simulate data uploaded to admin page and train a model
+        df = pd.read_csv(filepath_or_buffer='data/testData.csv')
+        testModel.dataset.putframe(df)
+        testModel.train_model()
+
+        # Assert if the file was created
+        file_exists = exists('UnitModel.h5')
+        self.assertTrue(file_exists, msg='File does not exist')
 
